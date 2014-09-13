65781084b84a885bfb70662a4c0cd5890f268470
KThrace <th.begeot@gmail.com>
2014-09-13 17:41:00 +0200

    productPanel

--- src/apps/Debiter.java
+++ src/apps/Debiter.java
@@ -1,5 +1,6 @@
 package apps;
 
+import java.awt.Color;
 import java.util.HashMap;
 
 import javax.swing.*;
@@ -11,9 +12,12 @@ public class Debiter extends JPanel{
 	private HashMap<JButton, String> product = new HashMap<JButton, String>();
 	
 	public Debiter(){
-		this.setLayout(new MigLayout("insets 0 0 0 0"));
+		this.setLayout(new MigLayout("insets 0 0 0 0, wrap"));
 		PSearchBar search = new PSearchBar();
+		ProductPane productPane = new ProductPane();
 		this.add(search,"center");
+		this.add(productPane,"w 100%, h 100%");
+		
 		
 	}
 	
